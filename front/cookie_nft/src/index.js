cec72a2e13033ed224d43b97d64cf5214ca4b1d7
WooJonghwa <wjh423@gmail.com>
2022-12-20 16:37:51 +0900

    Header 컴포넌트 생성 및 작성

--- front/cookie_nft/src/index.js
+++ front/cookie_nft/src/index.js
@@ -4,17 +4,18 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
     <Provider store={store}>
-      <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </Provider>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
