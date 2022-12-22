285f1163637ec19000858c753d5d4e6f942bc2e3
jinnyl22 <smile2374@gmail.com>
2022-12-22 09:28:05 +0900

    mint.js 충돌 부분 수정

--- front/cookie_nft/src/pages/Mint.js
+++ front/cookie_nft/src/pages/Mint.js
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 import { PickButton } from "../style/MintStyle";
-import ReactPlayer from "react-player";
 
 const Mint = () => {
   const [state, setState] = useState(undefined);
   return (
     <>
-      <PickButton onClick={}>뽑기</PickButton>
+      <PickButton>뽑기</PickButton>
     </>
   );
 };
