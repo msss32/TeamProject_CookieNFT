f0459afd8edd8c77fb710cc868ea0e8ddc8f479f
WooJonghwa <wjh423@gmail.com>
2022-12-22 17:29:16 +0900

    MyNFTstyle 수정

--- front/cookie_nft/src/pages/MyNFT.js
+++ front/cookie_nft/src/pages/MyNFT.js
@@ -2,30 +2,43 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import NFT from "../component/NFT";
 import { NFTaction } from "../redux/slice/NFTslice";
-import { NFTwrap } from "../style/MyNFTStyle";
-import { DarknessWitch, FireFairy, FrostQueen, MoonLight, SeaFairy, WindArcher } from "../test_img";
+import { NFTwrap, MyNFTwrap } from "../style/MyNFTStyle";
+import {
+    DarknessWitch,
+    FireFairy,
+    FrostQueen,
+    MoonLight,
+    SeaFairy,
+    WindArcher,
+} from "../test_img";
 
 const MyNFT = () => {
-  // test images
-  const myNFT = [DarknessWitch, FireFairy, FrostQueen, MoonLight, SeaFairy, WindArcher];
+    // test images
+    const myNFT = [DarknessWitch, FireFairy, FrostQueen, MoonLight, SeaFairy, WindArcher];
 
-  // Hook 할당
-  const dispatch = useDispatch();
+    // Hook 할당
+    const dispatch = useDispatch();
 
-  // redecer에서 내 NFT가 들어있는 배열 가져오기
-  const NFTs = useSelector((state) => state.MY_NFT.img);
+    // redecer에서 내 NFT가 들어있는 배열 가져오기
+    const NFTs = useSelector((state) => state.MY_NFT.img);
 
-  // 랜더링시 실행ㄱ
-  useEffect(() => {
-    dispatch(NFTaction.MY_NFT(myNFT));
-  }, []);
+    // 랜더링시 실행ㄱ
+    useEffect(() => {
+        // 소유한 NFT들 배열로 디스패치 // 현재는 test imgs
+        dispatch(NFTaction.MY_NFT(myNFT));
+    }, []);
 
-  return (
-    <div>
-      <div>보유중인 NFT</div>
-      <NFTwrap>{NFTs && NFTs.map((element, index) => <NFT key={index} index={index} element={element} />)}</NFTwrap>
-    </div>
-  );
+    return (
+        <MyNFTwrap>
+            <div style={{ fontFamily: "CookieRun", fontWeight: 600, fontSize: "3rem" }}>보유중인 NFT</div>
+            <NFTwrap>
+                {NFTs &&
+                    NFTs.map((element, index) => (
+                        <NFT key={index} index={index} element={element} />
+                    ))}
+            </NFTwrap>
+        </MyNFTwrap>
+    );
 };
 
 export default MyNFT;
--- front/cookie_nft/src/style/MyNFTStyle.js
+++ front/cookie_nft/src/style/MyNFTStyle.js
@@ -1,16 +1,28 @@
 import styled from "styled-components";
 
+const MyNFTwrap = styled.div`
+    width: 88.5vw;
+    margin: auto;
+    font-family: "CookieRun";
+    font-weight: 400;
+`;
+
 const NFTwrap = styled.div`
-    width: 100vw;
+    width: 100%;
     display: flex;
     flex-wrap: wrap;
 
     & > div {
-        margin: 30px;
+        margin: auto;
     }
-    & > div > img:hover {
-        cursor: pointer;
+    & > div > img {
+        transition: all 0.3s ease-in-out;
+        margin-top: 2vh;
+        &:hover {
+            cursor: pointer;
+            transform: scale(1.1);
+        }
     }
 `;
 
-export { NFTwrap };
+export { NFTwrap, MyNFTwrap };
