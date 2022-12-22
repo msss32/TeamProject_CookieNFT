09c584af4776ffbafd9a93e72f7e75155362cb48
jinnyl22 <smile2374@gmail.com>
2022-12-22 17:38:25 +0900

    nft 상세 페이지 작업중(디테일, 판매하기 버튼 추가)

--- front/cookie_nft/src/component/index.js
+++ front/cookie_nft/src/component/index.js
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import MainSlide from "./MainSlide";
 import SellNftList from "./SellNftList";
+import TransferForm from "./TransferForm";
 
-export { Header, MainSlide, SellNftList };
+export { Header, MainSlide, SellNftList, TransferForm };
--- front/cookie_nft/src/pages/MyNFT.js
+++ front/cookie_nft/src/pages/MyNFT.js
@@ -9,6 +9,10 @@ const MyNFT = () => {
   // test images
   const myNFT = [DarknessWitch, FireFairy, FrostQueen, MoonLight, SeaFairy, WindArcher];
 
+  const myNFTname = ["어둠마녀 쿠키", "불꽃정령 쿠키", "서리여왕 쿠키", "달빛술사 쿠키", "바다요정 쿠키", "바람궁수 쿠키"];
+
+  const myNFTprice = [0.05, 0.03, 0.04, 0.05, 0.04, 0.02];
+
   // Hook 할당
   const dispatch = useDispatch();
 
@@ -17,7 +21,7 @@ const MyNFT = () => {
 
   // 랜더링시 실행ㄱ
   useEffect(() => {
-    dispatch(NFTaction.MY_NFT(myNFT));
+    dispatch(NFTaction.MY_NFT({ img: myNFT, name: myNFTname, price: myNFTprice }));
   }, []);
 
   return (
