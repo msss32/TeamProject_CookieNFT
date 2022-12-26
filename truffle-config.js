a6d7cdae54466e41b970d3e71b386c2b645ac643
WooJonghwa <wjh423@gmail.com>
2022-12-26 12:19:19 +0900

    truffle-config.js 수정

--- truffle-config.js
+++ truffle-config.js
@@ -1,15 +1,27 @@
-module.exports = {
-  networks: {
-    development: {
-      host: "127.0.0.1", // 192.168.0.243
-      port: 8545, // 9000
-      network_id: 7722, // 1234
-    },
-  },
-  compilers: {
-    solc: {
-      version: "0.8.17",
-    },
-  },
-};
+require("dotenv").config();
 
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const infuraProjectId = process.env.INFURA_PROJECT_ID;
+
+module.exports = {
+    networks: {
+        development: {
+            host: "127.0.0.1", // "192.168.0.243"
+            port: 8545, // 9000
+            network_id: "*", // 1234
+        },
+        goerli: {
+            provider: () =>
+                new HDWalletProvider(
+                    process.env.DEV_MNEMONIC,
+                    "https://goerli.infura.io/v3/" + infuraProjectId
+                ),
+            network_id: 5,
+        },
+    },
+    compilers: {
+        solc: {
+            version: "0.8.17",
+        },
+    },
+};
